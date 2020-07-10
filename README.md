7632e0ad7400ec84ef50c143983c3b37586e4bad
lalabee <onlinelassie@gmail.com>
2020-07-09 18:51:02 -0700

    add reworked calculator.

---
+++ README.md
@@ -0,0 +1,5 @@
+# A Working Calculator
+## Authored by Epicodus, Erin, Jeffry, and Lauralee on July 9,2020 to complete an Epicodus assignment.
+## Made with HTML, jQuery, CSS, and Bootstrap version 3.3.7. 
+## Licenced by Mit Open Source guidelines.
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,50 @@
+// Interface Logic
+$(document).ready(function () {
+	$("form#calculator").submit(function () {
+		event.preventDefault();
+		const number1 = parseInt($("#input1").val());
+		const number2 = parseInt($("#input2").val());
+		const operator = $("input:radio[name=operator]:checked").val();
+		let result;
+		if (operator === "add") {
+			result = add(number1, number2);
+		} else if (operator === "subtract") {
+			result = subtract(number1, number2);
+		} else if (operator === "multiply") {
+			result = multiply(number1, number2);
+		} else if (operator === "divide") {
+			result = divide(number1, number2);
+		}
+		$("#output").text(result);
+	});
+});
+// Business Logic
 function add(number1, number2) {
   return number1 + number2;
-}
+};
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
-alert(result);
+function subtract(number1, number2) {
+  return number1 - number2;
+};
+
+function divide(number1, number2) {
+  return number1 / number2;
+};
+
+function multiply(number1, number2) {
+  return number1 * number2;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
 
